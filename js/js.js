4b10f6af3c88e95fc918c6faf15aab5d7158c8f4
MilanGentil <95704346+MilanGentil@users.noreply.github.com>
2022-05-13 09:08:55 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+var n = prompt("Coloca um numero"); //definição da variável "n" que vai guardar o número colocado pela pessoa
+while (n != null) //"n" não é igual a 0, por isso enquanto é igual a 1 ou outro número 
+{
+   i = 0 // i é igual a zero;
+   while (i <= n) //enquanto i<n 
+   {
+      document.write("<br>" + i + " " + i * i); //faz o dobro do número
+      i = i + 1; // i+1
+   }
+   n = prompt("Coloca um numero"); //coloca o número
+}
